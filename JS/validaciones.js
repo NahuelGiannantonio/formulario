3231560c02f4b615ec378a221b52fd1cec9979b5
Nahuel Giannantonio <nahuelgiannantonio@gmail.com>
2022-11-13 20:57:13 +0100

    version1

---
+++ JS/validaciones.js
@@ -0,0 +1,29 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]){
+        validadores[tipoDeInput](input);
+    }
+};
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input)
+};
+    
+function validarNacimiento(input){
+    const fechaCliente = new Date(input.value)
+    let mensaje = '';
+    mayorDeEdad(fechaCliente);
+    if(!mayorDeEdad(fechaCliente)){
+        mensaje = 'Debes ser mayor de edad'
+    };
+    input.setCustomValidity(mensaje);
+};
+
+function mayorDeEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18, 
+        fecha.getUTCMonth(), 
+        fecha.getUTCDate());
+    return fechaActual >= diferenciaFechas;
+};
