6ba6fb710e83627a6a86b2bc7f5712dee3e7d4b9
Nahuel Giannantonio <nahuelgiannantonio@gmail.com>
2022-11-15 14:26:40 +0100

    versionFinal

--- JS/validaciones.js
+++ JS/validaciones.js
@@ -3,12 +3,74 @@ export function valida(input) {
     if (validadores[tipoDeInput]){
         validadores[tipoDeInput](input);
     }
+    console.log(input.parentElement);
+    if (input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector('.input-message-error').innerHTML = '';
+    } else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector('.input-message-error').innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+    }
 };
+
+const tipoDeErrores = [
+    'valueMissing',
+    'typeMismatch',
+    'patternMismatch',
+    'customError',
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: 'Este campo no puede estar vacio'
+    },
+    email: {
+        valueMissing: 'Este campo no puede estar vacio',
+        typeMismatch: 'El correo no es valido'
+    },
+    password: {
+        valueMissing: 'Este campo no puede estar vacio',
+        patternMismatch: "Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales"
+    },
+    nacimiento: {
+        valueMissing: 'Este campo no puede estar vacio',
+        customError: 'Debes tener al menos 18 años'
+    },
+    numero: {
+        valueMissing: 'Este campo no puede estar vacio vacio',
+        patternMismatch: 'El formato requerido es XXXXXXXXXX 10 numeros'
+    },
+    direccion:{
+        valueMissing: 'Este campo no puede estar vacio',
+        patternMismatch: 'La direccion debe contener entre 10 a 40 caracteres'
+    },
+    ciudad:{
+        valueMissing: 'Este campo no puede estar vacio',
+        patternMismatch: 'La ciudad debe contener entre 10 a 40 caracteres'
+    },
+    estado:{
+        valueMissing: 'Este campo no puede estar vacio',
+        patternMismatch: 'El estado debe contener entre 10 a 40 caracteres'
+    },
+
+}
 
 const validadores = {
     nacimiento: (input) => validarNacimiento(input)
 };
     
+function mostrarMensajeDeError(tipoDeInput,input){
+    let mensaje = '';
+    tipoDeErrores.forEach(error =>{
+        if(input.validity[error]){
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    })
+
+
+    return mensaje
+}
+
 function validarNacimiento(input){
     const fechaCliente = new Date(input.value)
     let mensaje = '';
